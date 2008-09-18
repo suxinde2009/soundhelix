024c1278d6c24977f61accb35d07185e25aa6858
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2008-09-18 17:03:21 +0000

    Added support for specifying a list of chord tone offsets.

--- trunk/soundhelix/src/com/soundhelix/sequenceengine/PadSequenceEngine.java
+++ trunk/soundhelix/src/com/soundhelix/sequenceengine/PadSequenceEngine.java
@@ -12,11 +12,22 @@ import com.soundhelix.misc.Sequence;
 import com.soundhelix.misc.Track;
 import com.soundhelix.misc.Chord.ChordSubtype;
 import com.soundhelix.misc.Track.TrackType;
+import com.soundhelix.util.XMLUtils;
 
 /**
- * Implements a pad sequence engine using 3 or more voices. This engine
- * simply plays chords.
- * 
+ * Implements a pad sequence engine using 1 or more voices. This engine
+ * simply plays chords. The chord tones are selected by specifying a list
+ * of chord offsets. For example, offsets 0, 1 and 2 form a normal
+ * 3-tone chord. The chord can be "stretched" by using a wider offset range, e.g.,
+ * 0,1,2,3,4 which would add base and middle tone with increased octave to the
+ * normal chord tones.
+ * <br><br>
+ * <b>XML-Configuration</b>
+ * <table border=1>
+ * <tr><th>Tag</th> <th>#</th> <th>Example</th> <th>Description</th> <th>Required</th>
+ * <tr><td><code>offsets</code></td> <td></td> <td><code>0,1,2</code></td> <td>The list of offsets to use for playing chords.</td> <td>yes</td>
+ * </table>
+
  * @author Thomas Schürger (thomas@schuerger.com)
  */
 
@@ -27,14 +38,25 @@ public class PadSequenceEngine extends SequenceEngine {
 	
 	private static boolean obeyChordSubtypes = true;
 	
-	private static int voiceCount = 3;
 	private static int postPauseTicks = 1;
+	
+	private static int voiceCount = 3;
+	private int[] offsets;
 	
 	public PadSequenceEngine() {
 		super();
 	}
 	
-	public Track render(ActivityVector... activityVectors) {
+	public void setOffsets(int[] offsets) {
+		if(offsets.length == 0) {
+			throw(new RuntimeException("Array of offsets must not be empty"));
+		}
+		
+		this.offsets = offsets;
+    	this.voiceCount = offsets.length;
+	}
+	
+	public Track render(ActivityVector[] activityVectors) {
 		ActivityVector activityVector = activityVectors[0];
 
 		Track track = new Track(TrackType.MELODY);
@@ -58,21 +80,23 @@ public class PadSequenceEngine extends SequenceEngine {
         	
         	int activityLen = activityVector.getIntervalLength(tick);
         	
-        	int pos = 0;
+        	int shift = 0;
 
         	if(obeyChordSubtypes) {
         		if(chord.getSubtype() == ChordSubtype.BASE_4) {
-        			pos = 1;
+        			shift = 1;
         		} else if(chord.getSubtype() == ChordSubtype.BASE_6) {
-        			pos = -1;
+        			shift = -1;
         		}
         	}
-        	      	
+	
         	for(int i=0;i<voiceCount;i++) {
         		Sequence seq = track.get(i);
 
+        		int pos = offsets[i]+shift;
+        		
         		int octave = (pos >= 0 ? pos/3 : (pos-2)/3);
-        		int offset = (pos >= 0 ? pos%3 : (pos%3) + 3);
+        		int offset = ((pos%3)+3)%3;
         		
         		if(chord.isMajor()) {
         			seq.addNote(octave*12+majorTable[offset]+chord.getPitch(),Math.min(activityLen,len)-postPauseTicks);
@@ -100,5 +124,14 @@ public class PadSequenceEngine extends SequenceEngine {
 	}
 	
     public void configure(Node node,XPath xpath) throws XPathException {
+    	String[] offsetList = XMLUtils.parseString("offsets",node,xpath).split(",");
+    	
+    	int[] offsets = new int[offsetList.length];
+    	
+    	for(int i=0;i<offsetList.length;i++) {
+    		offsets[i] = Integer.parseInt(offsetList[i]);
+    	}
+    	
+    	setOffsets(offsets);
     }
 }

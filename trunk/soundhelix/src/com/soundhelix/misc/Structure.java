db45992813debf114f9d538217c47083ab8b3fd3
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2008-09-18 15:37:22 +0000

    Added documentation.

--- trunk/soundhelix/src/com/soundhelix/misc/Structure.java
+++ trunk/soundhelix/src/com/soundhelix/misc/Structure.java
@@ -8,14 +8,19 @@ import com.soundhelix.harmonyengine.HarmonyEngine;
  * 
  * The main quantization is a beat. Beats are divided into ticks (usually 4 ticks form a beat),
  * and beats are grouped into bars (usually 4 beats form a bar), and an integer number of bars
- * forms the song.
+ * forms the song. Ticks are the smallest quantization of a song.
  * 
  * The song signature is defined by specifying the total number of bars, the number of beats
  * per bar and the number of ticks per beat.
  * 
  * Each point in time within the song is addressed by its tick number (counted from 0). Beats, bars,
  * ticks within a beat and beats within a bar can be derived by using the ticks per beat and
- * the beats per bar.
+ * the beats per bar, if necessary.
+ *
+ * Each component must be ready to handle at least the following ticks per beat: 1, 2, 3, 4, 6, 8, 12, 16.
+ * For most music types, 3 (3/4 bars) or 4 (4/4 bars) ticks per beat will be fine. In order to mix 3/4 with 4/4
+ * notes, one might consider using 12 ticks per beat, so that quarter notes (3 ticks) and third notes
+ * (4 ticks) can be used at the same time. 
  * 
  * @author Thomas Schürger (thomas@schuerger.com)
  */

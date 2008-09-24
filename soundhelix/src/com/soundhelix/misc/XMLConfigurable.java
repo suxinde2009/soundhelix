a694965317b69cde8329d0618630fa807a5d3f5d
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2008-09-24 16:32:57 +0000

    Added documentation.

--- soundhelix/src/com/soundhelix/misc/XMLConfigurable.java
+++ soundhelix/src/com/soundhelix/misc/XMLConfigurable.java
@@ -7,7 +7,7 @@ import org.w3c.dom.Node;
 
 /**
  * Simple interface that adds XML-configurability to a class. XML
- * configuration is based on DOM.
+ * configuration is based on DOM and XPath.
  *
  * @author Thomas Schürger (thomas@schuerger.com)
  */
@@ -16,8 +16,15 @@ public interface XMLConfigurable {
 	/**
 	 * Configures the instance using the specified XML node. The method
 	 * should read all supported configuration tags and should call the
-	 * corresponding setter methods of the class. The method should
-	 * (but need not) use XPath to access the node's tags.
+	 * corresponding setter methods of the class. All settings of the
+	 * class that are settable using XML should also be settable by
+	 * using public methods (for example, if the beats per minute are
+	 * settable using an XML tag, then the class should also provide
+	 * a public method setBPM(), which does the same). XML configuration
+	 * can also be a recursive process, if XML settings contain nodes
+	 * that require XML configuration themselves, but this is up to the
+	 * implementation of this method. The method should (but need not)
+	 * use XPath to access the node's tags.
 	 * 
 	 * @param node the parent XML node of the configuration tags
 	 * @param xpath the XPath instance to use

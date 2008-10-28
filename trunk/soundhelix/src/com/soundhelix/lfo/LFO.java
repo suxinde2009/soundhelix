624cda5a7ae62ac706c6889a9a659e161b1aebcd
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2008-10-28 21:17:28 +0000

    Added documentation.

--- trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
+++ trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
@@ -7,9 +7,19 @@ import org.w3c.dom.Node;
 
 /**
  * Represents an abstract low frequency oscillator (LFO). The oscillator
- * speed can either be relative (depending on the BPM) or absolute (depending
- * on time). An LFO allows random access to its value given a tick or a
- * millisecond.
+ * speed can either be relative (depending on the BPM, e.g., a full rotation
+ * every 16 beats) or absolute (depending on time, e.g., a full rotation every
+ * 15 seconds). An LFO allows random access to its value given a tick or a millisecond.
+ * LFOs can be used for slowly changing certain settings, for example, for changing the
+ * filter cutoff frequency of a MIDI device.
+ * 
+ * This class implements the basic methods for setting the relative or absolute speed
+ * of the LFO as well as the minimum and maximum value to return. The type of LFO
+ * (e.g., sine, triangle, sawtooth or rectangle wave) must be implemented by subclasses.
+ * Subclasses can implement any type of waveform (for example, Bezier-spline interpolated
+ * random waveforms), but must return their results in a way so that they depend on the selected
+ * speed and that they are consistent (same method parameters must return same results for
+ * the same configuration of the same instance).
  * 
  * @author Thomas Schürger
  */

e40861eb3ce90e0007c85456b3724eebdd9a43ed
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2008-11-12 08:22:14 +0000

    Renamed terminology (absolute/relative to synchronized/unsynchronized) and corresponding methods. Added documentation.

--- trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
+++ trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
@@ -2,20 +2,20 @@ package com.soundhelix.lfo;
 
 
 /**
- * Represents an abstract low frequency oscillator (LFO). The oscillator
- * speed can either be relative (depending on the BPM, e.g., a full rotation
- * every 16 beats) or absolute (depending on time, e.g., a full rotation every
+ * Represents an abstract low frequency oscillator (LFO). The oscillator can either
+ * run in synchronized mode (depending on the BPM, e.g., a full rotation every
+ * 16 beats) or in unsynchronized mode (depending on time, e.g., a full rotation every
  * 15 seconds). An LFO allows random access to its value given a tick or a millisecond.
  * LFOs can be used for slowly changing certain settings, for example, for changing the
  * filter cutoff frequency of a MIDI device.
  * 
- * This class implements the basic methods for setting the relative or absolute speed
- * of the LFO as well as the minimum and maximum value to return. The type of LFO
- * (e.g., sine, triangle, sawtooth or rectangle wave) must be implemented by subclasses.
- * Subclasses can implement any type of waveform (for example, Bezier-spline interpolated
- * random waveforms), but must return their results in a way so that they depend on the selected
- * speed in a natural way and that they are consistent (same method parameters must
- * return same results for the same configuration of the same instance).
+ * This class implements the basic methods for setting the mode and speed of the LFO as well
+ * as the minimum and maximum value to return. The type of LFO (e.g., sine, triangle, sawtooth
+ * or rectangle wave) must be implemented by subclasses. Subclasses can implement any type
+ * of waveform (for example, Bezier-spline interpolated random waveforms), but must return
+ * their results so that they depend on the selected speed in a natural way and that they are
+ * consistent (same method parameters must return same results for the same configuration of
+ * the same instance).
  * 
  * LFO implementation must be able to handle small values for milli-RPM and milli-RPB
  * correctly (i.e., without returning jumpy/glitchy values).
@@ -34,7 +34,7 @@ public abstract class LFO {
 	protected int milliBPM;
 	protected double beatsPerTick;
 	
-	protected boolean relative;
+	protected boolean synchronizedMode;
 	
 	boolean speedSet;
 	
@@ -64,7 +64,7 @@ public abstract class LFO {
         
         double angle;
         
-        if(relative) {
+        if(synchronizedMode) {
             angle = Math.PI*(double)tick*beatsPerTick*(double)milliRPB*0.002d;
         } else {
             angle = 2.0d*Math.PI*(double)tick*beatsPerTick/(double)milliBPM*(double)milliRPM;            
@@ -88,7 +88,7 @@ public abstract class LFO {
 
         double angle;
         
-        if(relative) {
+        if(synchronizedMode) {
             angle = Math.PI*(double)milliBPM*(double)milliSecond*(double)milliRPB/30000000000d;
         } else {
             angle = Math.PI*(double)milliBPM*(double)milliSecond/(double)milliBPM*(double)milliRPM/30000000d;            
@@ -98,43 +98,55 @@ public abstract class LFO {
     }
     
 	/**
-	 * Sets the absolute speed of this LFO. Calling this method replaces
-	 * the previous absolute or relative speed.
+	 * Sets the the mode of the LFO to unsynchronized and uses the given speed.
+	 * Calling this method replaces the previously selected mode and speed.
 	 * 
 	 * @param milliRotationsPerMinute the number of millirotations per minute
 	 * @param milliBPM the number of milli-BPM
 	 * @param ticksPerBeat the number of ticks per beat
 	 */
 	
-	public void setAbsoluteSpeed(int milliRotationsPerMinute,int milliBPM,int ticksPerBeat) {
+	public void setUnsynchronizedSpeed(int milliRotationsPerMinute,int milliBPM,int ticksPerBeat) {
 		this.beatsPerTick = 1.0d/(double)ticksPerBeat;
 		this.milliRPM = milliRotationsPerMinute;
 		this.milliBPM = milliBPM;
-		this.relative = false;
+		this.synchronizedMode = false;
 		this.speedSet = true;
 	}
 	
 	/**
-	 * Sets the relative speed of this LFO. Calling this method replaces
-	 * the previous absolute or relative speed.
+	 * Sets the mode of the LFO to synchronized and uses the given speed.
+	 * Calling this method replaces the previously selected mode and speed.
 	 * 
 	 * @param milliRotationsPerBeat the millirotations per beat
      * @param milliBPM the number of milli-BPM
 	 * @param ticksPerBeat the number of ticks per beat
 	 */
 	
-	public void setRelativeSpeed(int milliRotationsPerBeat,int milliBPM,int ticksPerBeat) {	
+	public void setSynchronizedSpeed(int milliRotationsPerBeat,int milliBPM,int ticksPerBeat) {	
 		this.milliRPB = milliRotationsPerBeat;
 		this.milliBPM = milliBPM;
 		this.beatsPerTick = 1.0d/(double)ticksPerBeat;
-		this.relative = true;		
+		this.synchronizedMode = true;		
 		this.speedSet = true;
 	}
 
+	/**
+	 * Sets the minimum value to return.
+	 * 
+	 * @param minimum the minimum value
+	 */
+	
 	public void setMinimum(int minimum) {
 		this.minimum = minimum;
 	}
 
+	/**
+	 * Sets the maximum value to return.
+	 * 
+	 * @param maximum the maximum value
+	 */
+	
 	public void setMaximum(int maximum) {
 		this.maximum = maximum;
 	}

95e3079bc6a51b3866df761998a9ad8c2c3f0b7b
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2009-03-26 22:11:04 +0000

    Enhanced the usability of LFOs by adding more LFO speed modes.

--- trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
+++ trunk/soundhelix/src/com/soundhelix/lfo/LFO.java
@@ -2,39 +2,34 @@ package com.soundhelix.lfo;
 
 
 /**
- * Represents an abstract low frequency oscillator (LFO). The oscillator can either
- * run in synchronized mode (depending on the BPM, e.g., a full rotation every
- * 16 beats) or in unsynchronized mode (depending on time, e.g., a full rotation every
- * 15 seconds). An LFO allows random access to its value given a tick or a millisecond.
+ * Represents an abstract low frequency oscillator (LFO). The oscillator can run in one of four
+ * modes: synchronized to time (e.g, a full rotation every 5 seconds), synchronized to beat
+ * (e.g., a full rotation every 16 beats), synchronized to activity (e.g., a full rotation from
+ * activity start to activity end of an instrument) or synchronized to the song length (e.g., 2
+ * full rotations over the whole song). An LFO allows random access to its value given a tick.
  * LFOs can be used for slowly changing certain settings, for example, for changing the
  * filter cutoff frequency of a MIDI device.
  * 
  * This class implements the basic methods for setting the mode and speed of the LFO as well
- * as the minimum and maximum value to return. The type of LFO (e.g., sine, triangle, sawtooth
- * or rectangle wave) must be implemented by subclasses. Subclasses can implement any type
- * of waveform (for example, Bezier-spline interpolated random waveforms), but must return
- * their results so that they depend on the selected speed in a natural way and that they are
- * consistent (same method parameters must return same results for the same configuration of
- * the same instance).
+ * as the minimum and maximum value to return. In addition, the LFO's starting phase can be set.
+ * The type of LFO (e.g., sine, triangle, sawtooth or rectangle wave) must be implemented by
+ * subclasses. Subclasses can implement any type of waveform (for example, Bezier-spline interpolated
+ * random waveforms), but must return their results so that they depend on the selected speed in a
+ * natural way and that they are consistent (same method parameters must return same results for the
+ * same configuration of the same instance).
  * 
- * LFO implementation must be able to handle small values for milli-RPM and milli-RPB
+ * LFO implementation must be able to handle small values for their parameters and must return values
  * correctly (i.e., without returning jumpy/glitchy values).
  *
  * @author Thomas Schürger
  */
 
-// TODO: add support for specifying the initial phase
-
 public abstract class LFO {
 	protected int minimum;
 	protected int maximum;
 	
-	protected int milliRPM;
-	protected int milliRPB;
-	protected int milliBPM;
-	protected double beatsPerTick;
-	
-	protected boolean synchronizedMode;
+	private long microRotationsPerTick;
+	private long microRotationShift;
 	
 	boolean speedSet;
 	
@@ -62,73 +57,34 @@ public abstract class LFO {
             throw(new RuntimeException("LFO speed not set yet"));           
         }
         
-        double angle;
+        double angle = 2.0d*Math.PI*((double)tick*(double)microRotationsPerTick/1000000.0d + (double)microRotationShift/1000000d);
         
-        if(synchronizedMode) {
-            angle = Math.PI*(double)tick*beatsPerTick*(double)milliRPB*0.002d;
-        } else {
-            angle = 2.0d*Math.PI*(double)tick*beatsPerTick/(double)milliBPM*(double)milliRPM;            
-        }
-
         return minimum+(int)(0.5d+(double)maximum*(getValue(angle)));
     }
 
-	/**
-	 * Returns the LFO's value of the given millisecond.
-	 * 
-	 * @param milliSecond the millisecond
-	 * 
-	 * @return the LFO's value
-	 */
-	
-    public int getMilliSecondValue(int milliSecond) {
-        if(!speedSet) {
-            throw(new RuntimeException("LFO speed not set yet"));           
-        }
+	public void setBeatSpeed(int milliRotationsPerBeat,int ticksPerBeat,int milliBPM) {
+		this.microRotationsPerTick = milliRotationsPerBeat*1000L/ticksPerBeat; 
+		speedSet = true;
+	}
 
-        double angle;
-        
-        if(synchronizedMode) {
-            angle = Math.PI*(double)milliBPM*(double)milliSecond*(double)milliRPB/30000000000d;
-        } else {
-            angle = Math.PI*(double)milliBPM*(double)milliSecond/(double)milliBPM*(double)milliRPM/30000000d;            
-        }
+	public void setSongSpeed(int milliRotationsPerSong,int ticksPerSong,int milliBPM) {
+		this.microRotationsPerTick = milliRotationsPerSong*1000L/ticksPerSong; 
+		speedSet = true;
+	}
 
-        return minimum+(int)(0.5d+(double)maximum*(getValue(angle)));
-    }
-    
-	/**
-	 * Sets the the mode of the LFO to unsynchronized and uses the given speed.
-	 * Calling this method replaces the previously selected mode and speed.
-	 * 
-	 * @param milliRotationsPerMinute the number of millirotations per minute
-	 * @param milliBPM the number of milli-BPM
-	 * @param ticksPerBeat the number of ticks per beat
-	 */
-	
-	public void setUnsynchronizedSpeed(int milliRotationsPerMinute,int milliBPM,int ticksPerBeat) {
-		this.beatsPerTick = 1.0d/(double)ticksPerBeat;
-		this.milliRPM = milliRotationsPerMinute;
-		this.milliBPM = milliBPM;
-		this.synchronizedMode = false;
-		this.speedSet = true;
+	public void setActivitySpeed(int milliRotationsPerActivity,int startTick,int endTick,int milliBPM) {
+		this.microRotationsPerTick = milliRotationsPerActivity*1000L/(endTick-startTick);		
+		this.microRotationShift -= microRotationsPerTick*startTick;
+		speedSet = true;
+	}
+
+	public void setTimeSpeed(int milliRotationsPerSecond,int ticksPerBeat,int milliBPM) {
+		this.microRotationsPerTick = milliRotationsPerSecond*60000000L/milliBPM/ticksPerBeat;
+		speedSet = true;
 	}
 	
-	/**
-	 * Sets the mode of the LFO to synchronized and uses the given speed.
-	 * Calling this method replaces the previously selected mode and speed.
-	 * 
-	 * @param milliRotationsPerBeat the millirotations per beat
-     * @param milliBPM the number of milli-BPM
-	 * @param ticksPerBeat the number of ticks per beat
-	 */
-	
-	public void setSynchronizedSpeed(int milliRotationsPerBeat,int milliBPM,int ticksPerBeat) {	
-		this.milliRPB = milliRotationsPerBeat;
-		this.milliBPM = milliBPM;
-		this.beatsPerTick = 1.0d/(double)ticksPerBeat;
-		this.synchronizedMode = true;		
-		this.speedSet = true;
+	public void setPhase(int microRotations) {
+		this.microRotationShift = microRotations;
 	}
 
 	/**

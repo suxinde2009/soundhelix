c34a83064388fe31c476678c4bba9332af9957f5
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-02-22 20:36:06 +0000

    Added file.

---
+++ soundhelix/src/com/soundhelix/sequenceengine/AbstractFreeMultiPatternSequenceEngine.java
@@ -0,0 +1,90 @@
+package com.soundhelix.sequenceengine;
+
+import java.util.Random;
+
+import com.soundhelix.misc.ActivityVector;
+import com.soundhelix.misc.Pattern;
+import com.soundhelix.misc.Sequence;
+import com.soundhelix.misc.Track;
+import com.soundhelix.misc.Track.TrackType;
+
+/**
+ * Implements a sequence engine that repeats a set of user-specified patterns in a voice each.
+ * A pattern is a string containing any number of comma-separated integers and minus
+ * signs. Integers play the corresponding semitone (0 is C, 1 is C#, etc.; the numbers may also be
+ * negative). A minus sign is a pause.
+ *
+ * @author Thomas Schürger (thomas@schuerger.com)
+ */
+
+public abstract class AbstractFreeMultiPatternSequenceEngine extends AbstractSequenceEngine {
+	
+	protected Random random;
+	
+	private Pattern[] patterns;
+	
+	public AbstractFreeMultiPatternSequenceEngine() {
+		super();
+	}
+
+	public void setPatterns(String[] patternStrings) {
+		patterns = new Pattern[patternStrings.length];
+		
+		for(int i=0;i<patternStrings.length;i++) {
+			patterns[i] = Pattern.parseString(patternStrings[i]);
+		}
+	}
+	
+	public Track render(ActivityVector[] activityVectors) {
+		ActivityVector activityVector = activityVectors[0];
+
+        int ticks = structure.getTicks();
+        int patternCount = patterns.length;
+        
+		Sequence[] seqs = new Sequence[patternCount];
+		
+		for(int i=0;i<patternCount;i++) {
+			seqs[i] = new Sequence();
+		}
+
+		Track track = new Track(TrackType.MELODY);
+		
+       	for(int i=0;i<patterns.length;i++) {
+    		Sequence seq = seqs[i];
+    		Pattern pattern = patterns[i];
+    		int patternLength = pattern.size();
+    		int pos = 0;
+    		int tick = 0;
+
+			while(tick < ticks) {
+				Pattern.PatternEntry entry = pattern.get(pos%patternLength);
+        		int len = entry.getTicks();
+
+        		if(activityVector.isActive(tick)) {
+        			short vel = entry.getVelocity();
+
+        			if(entry.isPause()) {
+        				// add pause
+        				seq.addPause(len);
+        			} else {
+        				// normal note
+        				int pitch = entry.getPitch();
+
+        				boolean useLegato = entry.isLegato() ? pattern.isLegatoLegal(activityVector, tick+len, pos+1) : false;
+
+       					seq.addNote(pitch,len,vel,useLegato);
+        			}
+        		} else {
+        			// add pause
+        			seq.addPause(len);
+        		}
+
+        		tick += len;
+        		pos++;
+        	}
+    		track.add(seq);
+        }
+        
+        return track;
+	}
+}
---
+++ soundhelix/src/com/soundhelix/sequenceengine/FreeMultiPatternSequenceEngine.java
@@ -0,0 +1,58 @@
+package com.soundhelix.sequenceengine;
+
+import java.util.Random;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathException;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.soundhelix.util.XMLUtils;
+
+/**
+ * Implements a sequence engine that repeats a set of user-specified patterns in a voice each.
+ * A pattern is a string containing any number of comma-separated integers, minus and
+ * plus signs. Integers play the corresponding note of the chord (0 is the base
+ * note, 1 the middle note and so on; the numbers may also be negative). A minus
+ * sign is a pause. A plus sign plays a transition note between the current
+ * chord and the chord of the next non-transition tone that will be played. The
+ * pitch of the transition note is based on the base notes of the two chords.
+ * This can be used for funky base lines.
+ *
+ * @author Thomas Schürger (thomas@schuerger.com)
+ */
+
+public class FreeMultiPatternSequenceEngine extends AbstractFreeMultiPatternSequenceEngine {
+	
+	public FreeMultiPatternSequenceEngine() {
+		super();
+	}
+
+	public void configure(Node node,XPath xpath) throws XPathException {
+    	random = new Random(randomSeed);
+
+		NodeList patternsList = (NodeList)xpath.evaluate("patterns",node,XPathConstants.NODESET);
+
+		if(patternsList.getLength() == 0) {
+			throw(new RuntimeException("Need at least 1 list of patterns"));
+		}
+
+		Node patternsNode = patternsList.item(random.nextInt(patternsList.getLength()));
+		
+		NodeList patternList = (NodeList) xpath.evaluate("pattern", patternsNode,XPathConstants.NODESET);
+
+		if(patternList.getLength() == 0) {
+			throw(new RuntimeException("Need at least 1 pattern"));
+		}
+
+		String[] patterns = new String[patternList.getLength()];
+		
+		for(int i=0;i<patternList.getLength();i++) {
+			patterns[i] = XMLUtils.parseString(random,patternList.item(i),xpath);
+		}
+		
+		setPatterns(patterns);
+    }
+}

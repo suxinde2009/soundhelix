d43c78fd5372e3d9f29760a33a9b24df5e4f1ed1
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-02-22 20:36:06 +0000

    Added file.

---
+++ trunk/soundhelix/src/com/soundhelix/sequenceengine/FreePatternSequenceEngine.java
@@ -0,0 +1,48 @@
+package com.soundhelix.sequenceengine;
+
+import java.util.Random;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathException;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.soundhelix.util.XMLUtils;
+
+/**
+ * Implements a sequence engine that repeats user-specified patterns in a single
+ * voice. A pattern is a string containing any number of comma-separated integers, minus and
+ * plus signs. Integers play the corresponding note of the chord (0 is the base
+ * note, 1 the middle note and so on; the numbers may also be negative). A minus
+ * sign is a pause. A plus sign plays a transition note between the current
+ * chord and the chord of the next non-transition tone that will be played. The
+ * pitch of the transition note is based on the base notes of the two chords.
+ * This can be used for funky base lines.
+ * <br><br>
+ * <b>XML-Configuration</b>
+ * <table border=1>
+ * <tr><th>Tag</th> <th>#</th> <th>Example</th> <th>Description</th> <th>Required</th>
+ * <tr><td><code>pattern</code></td> <td>+</td> <td><code>0,-,-,-,1,-,2,-</code></td> <td>Sets the patterns to use. One of the patterns is selected at random.</td> <td>yes</td>
+ * <tr><td><code>obeyChordSubtype</code></td> <td>?</td> <td><code>yes</code></td> <td>Specifies whether to obey chord subtypes (defaults to no).</td> <td>no</td>
+ * </table>
+ * 
+ * @author Thomas Schürger (thomas@schuerger.com)
+ */
+
+public class FreePatternSequenceEngine extends AbstractFreeMultiPatternSequenceEngine {
+
+    public void configure(Node node,XPath xpath) throws XPathException {
+    	random = new Random(randomSeed);
+    	
+		NodeList nodeList = (NodeList)xpath.evaluate("pattern",node,XPathConstants.NODESET);
+
+		if(nodeList.getLength() == 0) {
+			throw(new RuntimeException("Need at least 1 pattern"));
+		}
+		
+		setPatterns(new String[] {
+				XMLUtils.parseString(random,nodeList.item(random.nextInt(nodeList.getLength())),xpath)});
+    }
+}

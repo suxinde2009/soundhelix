7d9aff4cc77a310250658baff870d2185c523889
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-06-10 19:14:21 +0000

    Cleaned up reflection-based class instance creation.

---
+++ soundhelix/src/com/soundhelix/util/ClassUtils.java
@@ -0,0 +1,32 @@
+package com.soundhelix.util;
+
+/**
+ * Implements some static methods for classes.
+ * 
+ * @author Thomas Schürger (thomas@schuerger.com)
+ */
+
+public class ClassUtils {
+	
+	private ClassUtils() {
+	}
+	
+	/**
+	 * Creates an instance of the given class name using the nullary constructor. The class is expected
+	 * to be of type T.
+	 * 
+	 * @param clazz the class
+	 * @param className the class name
+	 * @param <T> the expected class type
+	 * @return the instance
+	 * 
+	 * @throws ClassNotFoundException 
+	 * @throws InstantiationException 
+	 * @throws IllegalAccessException 
+	 */
+	
+    public static <T> T newInstance(String className, Class<T> clazz)
+    		throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		return Class.forName(className).asSubclass(clazz).newInstance();
+	}
+}

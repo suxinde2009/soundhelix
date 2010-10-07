4884b317c4339daf8a12989f5be204038a571ed3
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-10-07 19:42:00 +0000

    Added logger. Fixed typo setRandomSeed().

--- soundhelix/src/com/soundhelix/patternengine/AbstractPatternEngine.java
+++ soundhelix/src/com/soundhelix/patternengine/AbstractPatternEngine.java
@@ -1,14 +1,24 @@
 package com.soundhelix.patternengine;
 
-public abstract class AbstractPatternEngine implements PatternEngine {
+import org.apache.log4j.Logger;
+
+import com.soundhelix.misc.RandomSeedable;
+import com.soundhelix.misc.XMLConfigurable;
+
+public abstract class AbstractPatternEngine implements PatternEngine,RandomSeedable,XMLConfigurable {
 	protected long randomSeed;
 	
+	protected final Logger logger;
+	
+    public AbstractPatternEngine() {
+    	logger = Logger.getLogger(this.getClass());
+    }
+
 	public long getRandomSeed() {
 		return randomSeed;
 	}
 
-	public void setRandomSeed(long seed) {
+	public void setRandomSeed(long randomSeed) {
 		this.randomSeed = randomSeed;
 	}
-
 }

8eda06f80b59705bc2ea93aa03443d31c3f2972e
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-11-09 19:52:36 +0000

    Moved BuildConstants to new constants package.

--- trunk/soundhelix/src/com/soundhelix/constants/BuildConstants.java
+++ trunk/soundhelix/src/com/soundhelix/constants/BuildConstants.java
@@ -1,4 +1,4 @@
-package com.soundhelix.util;
+package com.soundhelix.constants;
 
 /**
  * Provides constants regarding the application build. These constants are modified automatically by the build
@@ -12,7 +12,7 @@ public class BuildConstants {
 	public static final String VERSION = "0.0.3";
 	
 	/** The Subversion revision of the build. */
-	public static final String REVISION = "326";
+	public static final String REVISION = "331";
 	
 	/** The date of the build. */
 	public static final String BUILD_DATE = "2010-11-09";

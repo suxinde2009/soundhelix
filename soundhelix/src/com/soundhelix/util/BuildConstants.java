3cf3cdc80ebd9cc895281323f7e03f52b30112c5
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2010-11-09 19:36:05 +0000

    Added build info.

---
+++ soundhelix/src/com/soundhelix/util/BuildConstants.java
@@ -0,0 +1,19 @@
+package com.soundhelix.util;
+
+/**
+ * Provides constants regarding the application build. These constants are modified automatically by the build
+ * process.
+ * 
+ * @author: Thomas Schürger (thomas@schuerger.com)
+ */
+
+public class BuildConstants {
+	/** The application version of the build. */
+	public static final String VERSION = "0.0.3";
+	
+	/** The Subversion revision of the build. */
+	public static final String REVISION = "326";
+	
+	/** The date of the build. */
+	public static final String BUILD_DATE = "2010-11-09";
+}

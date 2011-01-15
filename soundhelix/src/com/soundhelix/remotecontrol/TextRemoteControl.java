33f69563f2ca9104cbb911162690e88fd7665d31
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2011-01-15 21:54:12 +0000

    Improved song name generation to ensure that random strings not selected more than once.

--- soundhelix/src/com/soundhelix/remotecontrol/TextRemoteControl.java
+++ soundhelix/src/com/soundhelix/remotecontrol/TextRemoteControl.java
@@ -13,86 +13,92 @@ public abstract class TextRemoteControl implements RemoteControl {
 
 	private Player player;
 	
-		public void run() {
-		    boolean canExit = checkExitPermission(0);
-		    writeLine("Can exit: "+canExit);
-		    
-			while (true) {
-				try {
-					String line = readLine();
+	public void run() {
+	    boolean hasExitPermission = hasExitPermission(0);
 
-					if (line != null && !line.equals("")) {
-    					Player player = this.player;
-    
-    					if (line.startsWith("bpm ")) {
-    						if (player != null) {
-    							writeLine("Setting BPM");
-    
-    							player.setMilliBPM((int)(1000 * Double.parseDouble(line.substring(4))));
-    						}
-    					} else if (line.startsWith("transposition ")) {
-    							if (player != null && player instanceof MidiPlayer) {
-    								writeLine("Setting transposition");
-    
-    								((MidiPlayer)player).setTransposition(Integer.parseInt(line.substring(14)));
-    							}
-    					} else if (line.startsWith("groove ")) {
-    						if (player != null && player instanceof MidiPlayer) {
-    							writeLine("Setting groove");
-    
-    							((MidiPlayer)player).setGroove(line.substring(7));
-    						}
-    					} else if (line.equals("next")) {
-    						if (player != null) {
-    							writeLine("Next Song");
-    							player.abortPlay();
-    						}
-                        } else if (canExit && line.equals("quit")) {
-                            writeLine("Quitting");
-                            System.exit(0);
-    					} else if (line.equals("help")) {
-    						writeLine("\nAvailable commands");
-    						writeLine("------------------\n");
-    						writeLine("bpm <value>             Sets the BPM. Example: \"bpm 140\"");
-    						writeLine("transposition <value>   Sets the transposition. Example: \"transposition 70\"");
-    						writeLine("groove <value>          Sets the groove. Example: \"groove 130,70\"");
-    						writeLine("next                    Aborts playing and starts the next song. Example: \"next\"");
-                            if (canExit) {
-                                writeLine("quit                    Quits. Example: \"quit\"");
-                            }
-    						writeLine("");
-    					} else {
-    						writeLine("Invalid command. Type \"help\" for help.");
-    					}
-					} else {
-					    Thread.sleep(100);
-					}
-				} catch (Exception e) {
-					logger.error("Exception in console thread",e);
-				}
-			}
-		}
-	
+	    while (true) {
+	        try {
+	            String line = readLine();
+
+	            if (line != null && !line.equals("")) {
+	                Player player = this.player;
+
+	                if (line.startsWith("bpm ")) {
+	                    if (player != null) {
+	                        writeLine("Setting BPM");
+
+	                        player.setMilliBPM((int)(1000 * Double.parseDouble(line.substring(4))));
+	                    }
+	                } else if (line.startsWith("transposition ")) {
+	                    if (player != null && player instanceof MidiPlayer) {
+	                        writeLine("Setting transposition");
+
+	                        ((MidiPlayer)player).setTransposition(Integer.parseInt(line.substring(14)));
+	                    }
+	                } else if (line.startsWith("groove ")) {
+	                    if (player != null && player instanceof MidiPlayer) {
+	                        writeLine("Setting groove");
+
+	                        ((MidiPlayer)player).setGroove(line.substring(7));
+	                    }
+	                } else if (line.equals("next")) {
+	                    if (player != null) {
+	                        writeLine("Next Song");
+	                        player.abortPlay();
+	                    }
+	                } else if (hasExitPermission && line.equals("quit")) {
+	                    writeLine("Quitting");
+	                    System.exit(0);
+	                } else if (line.equals("help")) {
+	                    writeLine("\nAvailable commands");
+	                    writeLine("------------------\n");
+	                    writeLine("bpm <value>             Sets the BPM. Example: \"bpm 140\"");
+	                    writeLine("transposition <value>   Sets the transposition. Example: \"transposition 70\"");
+	                    writeLine("groove <value>          Sets the groove. Example: \"groove 130,70\"");
+	                    writeLine("next                    Aborts playing and starts the next song. Example: \"next\"");
+	                    if (hasExitPermission) {
+	                        writeLine("quit                    Quits. Example: \"quit\"");
+	                    }
+	                    writeLine("");
+	                } else {
+	                    writeLine("Invalid command. Type \"help\" for help.");
+	                }
+	            } else {
+	                Thread.sleep(100);
+	            }
+	        } catch (Exception e) {
+	            logger.error("Exception in console thread",e);
+	        }
+	    }
+	}
+
+	/**
+	 * Checks if the JVM environment supports calling System.exit(). Returns true if this
+	 * is the case, false otherwise.
+	 * 
+	 * @return true if System.exit() is supported, false otherwise
+	 */
+
+	private boolean hasExitPermission(int returnCode) {
+	    try {
+	        SecurityManager securitymanager = System.getSecurityManager();
+
+	        if(securitymanager != null) {
+	            securitymanager.checkExit(returnCode);
+	        }
+
+	        return true;
+	    } catch(AccessControlException e) {
+	        return false;
+	    }
+	}
+
 	public Player getPlayer() {
 		return player;
 	}
 
 	public void setPlayer(Player player) {
 		this.player = player;
-	}
-
-	private boolean checkExitPermission(int returnCode) {
-        try {
-            SecurityManager securitymanager = System.getSecurityManager();
-
-            if(securitymanager != null) {
-                securitymanager.checkExit(returnCode);
-            }
- 
-            return true;
-        } catch(AccessControlException e) {
-            return false;
-        }
 	}
 	
 	/**
--- soundhelix/src/com/soundhelix/songnameengine/CFGSongNameEngine.java
+++ soundhelix/src/com/soundhelix/songnameengine/CFGSongNameEngine.java
@@ -1,10 +1,11 @@
 package com.soundhelix.songnameengine;
 
-import java.util.Map;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+import java.util.List;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
@@ -23,19 +24,20 @@ import com.soundhelix.util.XMLUtils;
  */
 
 public class CFGSongNameEngine extends AbstractSongNameEngine {
-    private Map<String,String[]> variableMap;
+    private static Pattern variablePattern = Pattern.compile("\\$\\{(.*?)\\}");
+    private Map<String,RandomStringArray> variableMap;
     
     public String createSongName() {
         Random random = new Random(randomSeed);
 
-        String[] songNames = variableMap.get("songName");
+        RandomStringArray rsa = variableMap.get("songName");
 
-        if (songNames == null || songNames.length == 0) {
+        if (rsa == null || rsa.strings.length == 0) {
             throw new RuntimeException("Variable \"songName\" is undefined or has no values");
         }
 
-        String songName = songNames[random.nextInt(songNames.length)];
-        songName = StringUtils.replaceVariables(random, songName, variableMap);
+        String songName = getRandomString(random, rsa);
+        songName = replaceVariables(random, songName, variableMap);
         
         return StringUtils.capitalize(songName);
     }
@@ -46,10 +48,17 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
         NodeList nodeList = (NodeList)xpath.evaluate("variable",node,XPathConstants.NODESET);
         int variableCount = nodeList.getLength();
 
-        Map <String,String[]> variableMap = new HashMap<String,String[]>(variableCount);
+        Map <String,RandomStringArray> variableMap = new HashMap<String,RandomStringArray>(variableCount);
 
         for (int i = 0; i < variableCount; i++) {
             String name = XMLUtils.parseString(random,"attribute::name",nodeList.item(i),xpath);
+            boolean once;
+            
+            try {
+                once = XMLUtils.parseBoolean(random,"attribute::once",nodeList.item(i),xpath);
+            } catch(Exception e) {
+                once = true;
+            }
 
             if (variableMap.containsKey(name)) {
                 throw new RuntimeException("Variable \"" + name + "\" defined more than once");
@@ -58,17 +67,97 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
             String valueString = XMLUtils.parseString(random,nodeList.item(i),xpath);
             String[] values = valueString.split(",");
             
-            variableMap.put(name, values);
+            variableMap.put(name, new RandomStringArray(values, once));
         }
         
         setVariableMap(variableMap);
     }
 
-    public Map<String, String[]> getVariableMap() {
+    /**
+     * Recursively replaces all variables in the given string by a randomly chosen value from the corresponding variable
+     * map values.
+     * 
+     * @param random the random generator to use
+     * @param string the string to perform replacements in
+     * @param variableMap the variable map
+     */
+     
+    public static String replaceVariables(Random random, String string, Map<String,RandomStringArray> variableMap) {
+        if (string.indexOf('$') < 0) {
+            // string contains no variables, return it unchanged
+            return string;
+        }
+        
+        Matcher matcher = variablePattern.matcher(string);
+        StringBuffer sb = new StringBuffer();
+        
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, "");  
+            RandomStringArray rsa = variableMap.get(matcher.group(1));
+            
+            if (rsa == null) {
+                throw new RuntimeException("Variable \"" + matcher.group(1) + "\" is invalid");
+            }
+            
+            sb.append(getRandomString(random, rsa));  
+        }
+
+        matcher.appendTail(sb);  
+        
+        return replaceVariables(random, sb.toString(), variableMap);
+    }
+
+    /**
+     * Returns a random string from the given RandomStringArray.
+     * 
+     * @param random the random generator to use
+     * @param randomList the RandomList
+     *
+     * @return the random string
+     */
+    
+    private static String getRandomString(Random random,RandomStringArray randomList) {
+        if (randomList.selectOnce) {
+            String[] strings = randomList.strings;
+            int remaining = randomList.remaining;
+            
+            if (remaining <= 0) {
+                remaining = strings.length;
+            }
+            
+            int offset = random.nextInt(remaining);
+            remaining--;
+            
+            String value = strings[offset];
+            String otherValue = strings[remaining];
+            
+            strings[remaining] = value;
+            strings[offset] = otherValue;
+
+            randomList.remaining = remaining;
+            
+            return value;
+        } else {
+            return randomList.strings[random.nextInt(randomList.strings.length)];
+        }
+    }
+    
+    public Map<String, RandomStringArray> getVariableMap() {
         return variableMap;
     }
 
-    public void setVariableMap(Map<String, String[]> variableMap) {
+    public void setVariableMap(Map<String, RandomStringArray> variableMap) {
         this.variableMap = variableMap;
+    }
+    
+    public static class RandomStringArray {
+        public String[] strings;
+        public int remaining;
+        boolean selectOnce = true;
+        
+        public RandomStringArray(String[] strings, boolean selectOnce) {
+            this.strings = strings;
+            this.selectOnce = selectOnce;
+        }
     }
 }

0196651bb3782b46ee4e3709bf04fe76f55adccb
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2011-01-28 11:34:54 +0000

    Made applet window resizable. Cleaned up code.

--- trunk/soundhelix/src/com/soundhelix/SoundHelixApplet.java
+++ trunk/soundhelix/src/com/soundhelix/SoundHelixApplet.java
@@ -1,8 +1,11 @@
 package com.soundhelix;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.net.URL;
+import java.awt.Frame;
 import java.util.Random;
 
 import javax.swing.JApplet;
@@ -35,41 +38,59 @@ import com.soundhelix.util.SongUtils;
 public class SoundHelixApplet extends JApplet implements Runnable {
 
     /** The root logger. */
-	private static Logger rootLogger = Logger.getRootLogger();
-	
-	/** The logger. */
-	private static Logger logger = Logger.getLogger(SoundHelixApplet.class);
+    private static Logger rootLogger = Logger.getRootLogger();
+    
+    /** The logger. */
+    private static Logger logger = Logger.getLogger(SoundHelixApplet.class);
 
-	/** The remote control. */
-	private TextRemoteControl remoteControl;
-	
-	public static void main(String[] args) {
-		new SoundHelixApplet().start();
-	}
-	
-	@Override
-	public void start() {
-		super.start();
+    /** The remote control. */
+    private TextRemoteControl remoteControl;
+    
+    /**
+     * Starts the applet.
+     * 
+     * @param args the arguments
+     */
+    
+    public static void main(String[] args) {
+        new SoundHelixApplet().start();
+    }
+    
+    @Override
+    public void start() {
+        Component parent = this;
 
+        while (parent.getParent() != null) {
+            parent = parent.getParent();
+        }
+        
+        if (parent instanceof Frame) {
+            if (!((Frame) parent).isResizable()) {
+                ((Frame) parent).setResizable(true);
+            }
+        }
+        
         setLayout(new BorderLayout());
 
-	    JTextArea outputTextArea;
-		outputTextArea = new JTextArea();
-		Font font = new Font("Monospaced", Font.PLAIN, 11);
+        JTextArea outputTextArea;
+        outputTextArea = new JTextArea();
+        Font font = new Font("Monospaced", Font.PLAIN, 11);
         outputTextArea.setFont(font);
         outputTextArea.setEditable(false);
         
-		add(new JScrollPane(outputTextArea), BorderLayout.CENTER);
+        add(new JScrollPane(outputTextArea), BorderLayout.CENTER);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		panel.add(new JLabel(" Command: "), BorderLayout.WEST);
-		JTextField inputTextField = new JTextField();
-		inputTextField.requestFocus();
-		panel.add(inputTextField, BorderLayout.CENTER);
-		add(panel, BorderLayout.SOUTH);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(new JLabel(" Command: "), BorderLayout.WEST);
+        JTextField inputTextField = new JTextField();
+        inputTextField.requestFocus();
+        panel.add(inputTextField, BorderLayout.CENTER);
+        add(panel, BorderLayout.SOUTH);
 
-		remoteControl = new SwingRemoteControl(inputTextField, outputTextArea);
+        super.start();
+
+        remoteControl = new SwingRemoteControl(inputTextField, outputTextArea);
 
         // launch console thread with normal priority
         Thread consoleThread = new Thread(new Runnable() {
@@ -81,79 +102,91 @@ public class SoundHelixApplet extends JApplet implements Runnable {
         consoleThread.setPriority(Thread.NORM_PRIORITY);
         consoleThread.start();
 
-		// launch song generation and playing thread with high priority
-		Thread playerThread = new Thread(this, "Player");
-		playerThread.setPriority(Thread.MAX_PRIORITY);
-		playerThread.start();
-	}
+        // launch song generation and playing thread with high priority
+        Thread playerThread = new Thread(this, "Player");
+        playerThread.setPriority(Thread.MAX_PRIORITY);
+        playerThread.start();
+    }
 
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-		super.stop();
-	}
-	
-	public void run() {
-		initializeLog4j();
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        super.stop();
+    }
+    
+    /**
+     * Implements the player thread functionality.
+     */
+    
+    public void run() {
+        initializeLog4j();
 
         logger.info("SoundHelix " + BuildConstants.VERSION + " (r" + BuildConstants.REVISION + "), built on "
                   + BuildConstants.BUILD_DATE);
 
-		Random random = new Random();
-	
-		for (;;) {
-			try {
-				URL url = new URL(getDocumentBase(), "examples/SoundHelix-Piano.xml");
-				Player player = SongUtils.generateSong(url, random.nextLong());
-				
-				player.open();
+        Random random = new Random();
+    
+        for (;;) {
+            try {
+                URL url = new URL(getDocumentBase(), "examples/SoundHelix-Piano.xml");
+                Player player = SongUtils.generateSong(url, random.nextLong());
+                
+                player.open();
                 remoteControl.setPlayer(player);
-				player.play();
+                player.play();
                 remoteControl.setPlayer(null);
-				player.close();
-			} catch (Exception e) {
-				logger.debug("Exception occurred", e);
-			}
-			
-			try {
-				Thread.sleep(1000);
-			} catch (Exception e) {}
-		}
-	}
+                player.close();
+            } catch (Exception e) {
+                logger.debug("Exception occurred", e);
+            }
+            
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {}
+        }
+    }
 
+    /**
+     * Initializes log4j.
+     */
+    
     private void initializeLog4j() {
         Layout layout = new PatternLayout("%d{ISO8601} %-5p [%t] %c{1}: %m%n");
-		Appender consoleAppender = new TextRemoteControlAppender(layout, remoteControl);
-		rootLogger.addAppender(consoleAppender);
-		rootLogger.setLevel(Level.DEBUG);
+        Appender consoleAppender = new TextRemoteControlAppender(layout, remoteControl);
+        rootLogger.addAppender(consoleAppender);
+        rootLogger.setLevel(Level.DEBUG);
     }
-	
-	public static class TextRemoteControlAppender extends AppenderSkeleton {
-	    /** The layout. */
-		private Layout layout;
-		
-		/** The remote control. */
-		private TextRemoteControl remoteControl;
-		
-		public TextRemoteControlAppender(Layout layout, TextRemoteControl remoteControl) {
-			super();
-			this.layout = layout;
-			this.remoteControl = remoteControl;
-		}
-		
-		public boolean requiresLayout() {
-			return true;
-		}
-		
-		public void append(LoggingEvent event) {
-			String message = layout.format(event);
-			
-			if (message.endsWith("\r\n")) {
-			    message = message.substring(0, message.length() - 2);
-			} else if (message.endsWith("\n")) {
-                message = message.substring(0, message.length() - 1);			    
-			}
-			
+    
+    /**
+     * Implements a log4j appender that uses the TextRemoteControl for logging.
+     */
+    
+    public static class TextRemoteControlAppender extends AppenderSkeleton {
+        /** The layout. */
+        private Layout layout;
+        
+        /** The remote control. */
+        private TextRemoteControl remoteControl;
+        
+        public TextRemoteControlAppender(Layout layout, TextRemoteControl remoteControl) {
+            super();
+            this.layout = layout;
+            this.remoteControl = remoteControl;
+        }
+        
+        public boolean requiresLayout() {
+            return true;
+        }
+        
+        public void append(LoggingEvent event) {
+            String message = layout.format(event);
+            
+            if (message.endsWith("\r\n")) {
+                message = message.substring(0, message.length() - 2);
+            } else if (message.endsWith("\n")) {
+                message = message.substring(0, message.length() - 1);                
+            }
+            
             remoteControl.writeLine(message);
 
             if (layout.ignoresThrowable()) {
@@ -165,9 +198,9 @@ public class SoundHelixApplet extends JApplet implements Runnable {
                     }
                 }
             }
-		}
-		
-		public void close() {
-		}
-	}
+        }
+        
+        public void close() {
+        }
+    }
 }

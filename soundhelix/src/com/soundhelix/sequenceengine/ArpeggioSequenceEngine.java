7a93ca4ef5c53eca4712d9aeaa657c4fcc9d93cf
TranceTip <TranceTip@2a610bfd-e773-4f87-915c-491094d88175>
2011-04-25 20:56:32 +0000

    Reimplemented the ArpeggioSequenceEngine to use PatternEngines.

--- soundhelix/src/com/soundhelix/sequenceengine/ArpeggioSequenceEngine.java
+++ soundhelix/src/com/soundhelix/sequenceengine/ArpeggioSequenceEngine.java
@@ -1,5 +1,7 @@
 package com.soundhelix.sequenceengine;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import javax.xml.xpath.XPath;
@@ -12,10 +14,12 @@ import org.w3c.dom.NodeList;
 import com.soundhelix.harmonyengine.HarmonyEngine;
 import com.soundhelix.misc.ActivityVector;
 import com.soundhelix.misc.Chord;
+import com.soundhelix.misc.Chord.ChordSubtype;
+import com.soundhelix.misc.Pattern;
 import com.soundhelix.misc.Sequence;
 import com.soundhelix.misc.Track;
-import com.soundhelix.misc.Chord.ChordSubtype;
 import com.soundhelix.misc.Track.TrackType;
+import com.soundhelix.patternengine.PatternEngine;
 import com.soundhelix.util.XMLUtils;
 
 /**
@@ -31,74 +35,103 @@ import com.soundhelix.util.XMLUtils;
 // TODO: allow specifying velocities in arpeggio patterns (like in the PatternSequenceEngine)
 
 public class ArpeggioSequenceEngine extends AbstractSequenceEngine {
-	private static final int[] MAJOR_TABLE = new int[] {0, 4, 7};
-	private static final int[] MINOR_TABLE = new int[] {0, 3, 7};
+	protected static final int[] MAJOR_TABLE = new int[] {0, 4, 7};
+	protected static final int[] MINOR_TABLE = new int[] {0, 3, 7};
 
-	private static boolean obeyChordSubtype = true;
-
-	private int[][] patterns;
+	protected Random random;	
+	protected boolean obeyChordSubtype;
+	
+	private Pattern[] patterns;
 
 	public ArpeggioSequenceEngine() {
 		super();
 	}
+
+	public void setPatterns(Pattern[] patterns) {
+		this.patterns = patterns;
+	}
 	
+	public void setObeyChordSubtype(boolean obeyChordSubtype) {
+		this.obeyChordSubtype = obeyChordSubtype;
+	}
+
 	public Track render(ActivityVector[] activityVectors) {
 		ActivityVector activityVector = activityVectors[0];
-		
-        Sequence seq = new Sequence();
-        HarmonyEngine ce = structure.getHarmonyEngine();
+
+        HarmonyEngine harmonyEngine = structure.getHarmonyEngine();        
         
         int tick = 0;
-        
-        while (tick < structure.getTicks()) {
-        	Chord chord = ce.getChord(tick);
-        	int len = ce.getChordTicks(tick);
-        	
-        	// get arpeggio table for current chord section length
-			int[] pattern = getArpeggioPattern(len);
-        	int patternLen = pattern.length;
-			
-        	for (int i = 0; i < len; i++) {
+
+        Sequence seq = new Sequence();
+        int ticks = structure.getTicks();
+
+        while (tick < ticks) {
+        	Chord chord = harmonyEngine.getChord(tick);
+        	int chordTicks = harmonyEngine.getChordTicks(tick);
+
+        	Pattern pattern = getArpeggioPattern(chordTicks);
+        	int patternLength = pattern.size();
+            int pos = 0;
+
+        	for (int t = 0; t < chordTicks;) {
+        		Pattern.PatternEntry entry = pattern.get(pos % patternLength);
+        		int len = entry.getTicks();
+
+        		if (t + len > chordTicks) {
+        			len = chordTicks - t;
+        		}
+        		
         		if (activityVector.isActive(tick)) {
-        			// add next note for major/minor chord
-        			
-        			int value = pattern[i % patternLen];
-        			
-        			if (value == Integer.MIN_VALUE) {
-            			// add pause
-            			seq.addPause(1);
-        				continue;
-        			}
-        			
-        			if (obeyChordSubtype) {
-        				if (chord.getSubtype() == ChordSubtype.BASE_4) {
-        					value++;
-        				} else if (chord.getSubtype() == ChordSubtype.BASE_6) {
-        					value--;
+        			short vel = entry.getVelocity();
+
+        			if (entry.isPause()) {
+        				// add pause
+        				seq.addPause(len);
+        			} else {
+        				// normal note
+        				int value = entry.getPitch();
+
+        				if (obeyChordSubtype) {
+        					if (chord.getSubtype() == ChordSubtype.BASE_4) {
+        						value++;
+        					} else if (chord.getSubtype() == ChordSubtype.BASE_6) {
+        						value--;
+        					}
+        				}
+
+        				// split value into octave and offset
+        				// we add 3 to avoid modulo and division issues with
+        				// negative values
+
+        				int octave = value >= 0 ? value / 3 : (value - 2) / 3;
+        				int offset = ((value % 3) + 3) % 3;
+
+        				boolean useLegato = entry.isLegato()
+        						? pattern.isLegatoLegal(activityVector, tick + len, pos + 1) : false;
+
+        				if (chord.isMajor()) {
+        					seq.addNote(octave * 12 + MAJOR_TABLE[offset] + chord.getPitch(), len, vel, useLegato);
+        				} else {
+        					seq.addNote(octave * 12 + MINOR_TABLE[offset] + chord.getPitch(), len, vel, useLegato);
         				}
         			}
-        			
-        			int octave = value >= 0 ? value / 3 : (value - 2) / 3;
-        			int offset = ((value % 3) + 3) % 3;
-        			
-        	 	    if (chord.isMajor()) {
-        			    seq.addNote(octave * 12 + MAJOR_TABLE[offset] + chord.getPitch(), 1);
-        		    } else {
-           			    seq.addNote(octave * 12 + MINOR_TABLE[offset] + chord.getPitch(), 1);
-        		    }
         		} else {
         			// add pause
-        			seq.addPause(1);
+        			seq.addPause(len);
         		}
-         	}
-        	
-        	tick += len;
+
+        		t += len;
+        		tick += len;
+        		pos++;
+        	}
         }
         
-		Track track = new Track(TrackType.MELODY);
-        track.add(seq);
+        Track track = new Track(TrackType.MELODY);
+    	track.add(seq);
+    			
         return track;
 	}
+	
 
 	/**
 	 * Returns an optimal arpeggio pattern for the given length,
@@ -112,7 +145,7 @@ public class ArpeggioSequenceEngine extends AbstractSequenceEngine {
 	 * @return the arpeggio pattern
 	 */
 	
-	private int[] getArpeggioPattern(int len) {
+	private Pattern getArpeggioPattern(int len) {
 		// slow implementation, but this method is only called
 		// once per chord and we normally don't have a whole lot of patterns
 		
@@ -124,7 +157,7 @@ public class ArpeggioSequenceEngine extends AbstractSequenceEngine {
 		int maxIndexLen = -1;
 		
 		for (int i = 0; i < patterns.length; i++) {
-			int l = patterns[i].length;
+			int l = patterns[i].getTicks();
 			
 			if (l >= len && l < bestIndexLen) {
 				bestIndex = i;
@@ -144,41 +177,55 @@ public class ArpeggioSequenceEngine extends AbstractSequenceEngine {
 		}
 	}
 	
-	public void setPatterns(int[][] patterns) {
-		this.patterns = patterns;
-	}
-	
-    public void configure(Node node, XPath xpath) throws XPathException {
+   public void configure(Node node, XPath xpath) throws XPathException {
     	Random random = new Random(randomSeed);
     	
-		NodeList nodeList = (NodeList) xpath.evaluate("pattern", node, XPathConstants.NODESET);
-
-		int patterns = nodeList.getLength();
+		NodeList patternEnginesNodes = (NodeList) xpath.evaluate("patternEngines", node, XPathConstants.NODESET);
 		
+		int patternEnginesCount = patternEnginesNodes.getLength();
+
+		if (patternEnginesCount == 0) {
+			throw new RuntimeException("Need at least 1 patternEngines tag");
+		}
+
+		Node patternEnginesNode = patternEnginesNodes.item(random.nextInt(patternEnginesCount));
+		
+		NodeList nodeList = (NodeList) xpath.evaluate("patternEngine", patternEnginesNode, XPathConstants.NODESET);
+
 		if (nodeList.getLength() == 0) {
-			throw new RuntimeException("Need at least 1 pattern");
+			throw new RuntimeException("Need at least 1 patternEngine");
 		}
 		
-		int[][] array = new int[patterns][];
+		try {
+			setObeyChordSubtype(XMLUtils.parseBoolean(random, "obeyChordSubtype", node, xpath));
+		} catch (Exception e) {}
 		
-		for (int i = 0; i < patterns; i++) {
-			String pattern = XMLUtils.parseString(random, nodeList.item(i), xpath);
+		int patternEngineCount = nodeList.getLength();
 
-			String[] p = pattern.split(",");
+		Map<Integer, Boolean> patternLengthMap = new HashMap<Integer, Boolean>(patternEngineCount);
+		
+		Pattern[] patterns = new Pattern[patternEngineCount];
+
+		for (int i = 0; i < patternEngineCount; i++) {
+			PatternEngine patternEngine;
+
+			try {
+				patternEngine = XMLUtils.getInstance(PatternEngine.class, nodeList.item(i),
+						xpath, randomSeed, i);
+			} catch (Exception e) {
+				throw new RuntimeException("Error instantiating PatternEngine", e);
+			}
+		
+			patterns[i] = patternEngine.render("");
+			int ticks = patterns[i].getTicks();
 			
-			int[] l = new int[p.length];
-			
-			for (int k = 0; k < p.length; k++) {
-				if (p[k].equals("-")) {
-					l[k] = Integer.MIN_VALUE;
-				} else {
-				    l[k] = Integer.parseInt(p[k]);
-				}
+			if (patternLengthMap.containsKey(ticks)) {
+				throw new RuntimeException("Another pattern with " + ticks + " ticks was already provided");
 			}
 			
-			array[i] = l;
+			patternLengthMap.put(ticks, true);
 		}
 		
-		setPatterns(array);   
+		setPatterns(patterns);
     }
 }

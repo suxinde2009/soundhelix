4a0e3a68cc48985b7e2fd7a606427890daa2c912
TranceTip <TranceTip@2a610bfd-e773-4f87-915c-491094d88175>
2011-04-25 20:57:53 +0000

    Fixed random seed augmentation.

--- trunk/soundhelix/src/com/soundhelix/sequenceengine/PatternSequenceEngine.java
+++ trunk/soundhelix/src/com/soundhelix/sequenceengine/PatternSequenceEngine.java
@@ -51,7 +51,7 @@ public class PatternSequenceEngine extends AbstractMultiPatternSequenceEngine {
 		try {
 			int i = random.nextInt(nodeList.getLength());
 			patternEngine = XMLUtils.getInstance(PatternEngine.class, nodeList.item(i),
-					xpath, randomSeed ^ 47351842858L, i);
+					xpath, randomSeed, i);
 		} catch (Exception e) {
 			throw new RuntimeException("Error instantiating PatternEngine", e);
 		}

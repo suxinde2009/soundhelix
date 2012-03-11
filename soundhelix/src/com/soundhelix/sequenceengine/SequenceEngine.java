ac514e7b08dd462316cabba5351b5378b436b230
TranceTip <TranceTip@2a610bfd-e773-4f87-915c-491094d88175>
2012-03-11 16:42:47 +0000

    Fixed documentation.

--- soundhelix/src/com/soundhelix/sequenceengine/SequenceEngine.java
+++ soundhelix/src/com/soundhelix/sequenceengine/SequenceEngine.java
@@ -9,8 +9,6 @@ import com.soundhelix.misc.XMLConfigurable;
 /**
  * Represents a generator for sequences. Generated sequences are combined into a track.
  * 
- * @see Sequence
- * 
  * @author Thomas Schuerger (thomas@schuerger.com)
  */
 
@@ -26,7 +24,7 @@ public interface SequenceEngine extends XMLConfigurable, RandomSeedable {
     /**
      * Renders one or more sequences (i.e., voices) as a track. The method should check the given ActivityVectors to decide when to insert notes and
      * when to insert pauses. The method should also take care that played notes are not sustained beyond inactive intervals of the ActivityVector
-     * (however, it is not strictly forbidden to do so). The returned list must contain at least one sequence. The length of each sequence must match
+     * (however, it is not strictly forbidden to do so). The returned track must contain at least one sequence. The length of each sequence must match
      * the length of the song. The method must take the song's HarmonyEngine into consideration.
      * 
      * @param activityVectors the activity vectors
@@ -38,7 +36,7 @@ public interface SequenceEngine extends XMLConfigurable, RandomSeedable {
 
     /**
      * Returns the required number of ActivityVectors. For most implementations, this will be 1, but certain implementations, like for
-     * multi-instrument sequences, more than 1 might be required. Subclasses should override this.
+     * multi-instrument sequences, more than 1 might be required.
      * 
      * @return the number of ActivityVectors
      */

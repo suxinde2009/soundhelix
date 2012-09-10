63ca1d69db810e34769fb8405bdc06f75b86b619
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2012-09-10 07:23:58 +0000

    Removed ununsed code.

--- trunk/soundhelix/src/com/soundhelix/patternengine/StringPatternEngine.java
+++ trunk/soundhelix/src/com/soundhelix/patternengine/StringPatternEngine.java
@@ -24,7 +24,7 @@ public class StringPatternEngine extends AbstractPatternEngine {
     private String patternString;
 
     public Pattern render(String wildcardString) {
-        return parseString(patternString, wildcardString);
+        return Pattern.parseString(patternString, wildcardString);
     }
 
     public void configure(Node node, XPath xpath) throws XPathException {
@@ -37,45 +37,6 @@ public class StringPatternEngine extends AbstractPatternEngine {
         }
 
         setPatternString(XMLUtils.parseString(random, nodeList.item(random.nextInt(nodeList.getLength())), xpath));
-    }
-
-    private static Pattern parseString(String patternString, String wildcardString) {
-        if (wildcardString == null) {
-            wildcardString = "";
-        }
-
-        PatternEntry[] pattern;
-
-        String[] p = patternString.split(",");
-        int len = p.length;
-
-        pattern = new PatternEntry[len];
-
-        // format: offset/ticks:velocity or offset~/ticks:velocity
-
-        for (int i = 0; i < len; i++) {
-            String[] a = p[i].split(":");
-            short v = a.length > 1 ? Short.parseShort(a[1]) : Short.MAX_VALUE;
-            String[] b = a[0].split("/");
-            int t = b.length > 1 ? Integer.parseInt(b[1]) : 1;
-
-            boolean legato = b[0].endsWith("~");
-
-            if (legato) {
-                // cut off legato character
-                b[0] = b[0].substring(0, b[0].length() - 1);
-            }
-
-            if (b[0].equals("-")) {
-                pattern[i] = new Pattern.PatternEntry(t);
-            } else if (b[0].length() == 1 && wildcardString.indexOf(b[0]) >= 0) {
-                pattern[i] = new PatternEntry(b[0].charAt(0), v, t, legato);
-            } else {
-                pattern[i] = new PatternEntry(Integer.parseInt(b[0]), v, t, legato);
-            }
-        }
-
-        return new Pattern(pattern);
     }
 
     public String getPatternString() {

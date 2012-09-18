cbf170638d5309cbfba723d3fbedbfe550f77d29
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2012-09-18 13:58:06 +0000

    Refactored code.

--- trunk/soundhelix/src/com/soundhelix/songnameengine/CFGSongNameEngine.java
+++ trunk/soundhelix/src/com/soundhelix/songnameengine/CFGSongNameEngine.java
@@ -1,6 +1,8 @@
 package com.soundhelix.songnameengine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.regex.Matcher;
@@ -18,7 +20,7 @@ import com.soundhelix.util.XMLUtils;
 
 /**
  * Implements a flexible song name engine based on a context-free grammar.
- * 
+ *
  * @author Thomas Schuerger (thomas@schuerger.com)
  */
 
@@ -27,7 +29,7 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
     private static Pattern variablePattern = Pattern.compile("\\$\\{(.*?)\\}");
 
     /** The variable map. */
-    private Map<String, RandomStringArray> variableMap;
+    private Map<String, RandomStrings> variableMap;
 
     /** The start variable. */
     private String startVariable = "songName";
@@ -39,13 +41,13 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
     public String createSongName() {
         Random random = new Random(randomSeed);
 
-        RandomStringArray rsa = variableMap.get(startVariable);
+        RandomStrings strings = variableMap.get(startVariable);
 
-        if (rsa == null || rsa.strings.length == 0) {
+        if (strings == null || strings.size() == 0) {
             throw new RuntimeException("Variable \"" + startVariable + "\" is undefined or has no values");
         }
 
-        String songName = getRandomString(random, rsa);
+        String songName = strings.next(random);
         songName = replaceVariables(random, songName, variableMap);
 
         return StringUtils.capitalize(songName);
@@ -58,7 +60,7 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
         NodeList nodeList = (NodeList) xpath.evaluate("variable", node, XPathConstants.NODESET);
         int variableCount = nodeList.getLength();
 
-        Map<String, RandomStringArray> variableMap = new HashMap<String, RandomStringArray>(variableCount);
+        Map<String, RandomStrings> variableMap = new HashMap<String, RandomStrings>(variableCount);
 
         for (int i = 0; i < variableCount; i++) {
             String name = XMLUtils.parseString(random, "attribute::name", nodeList.item(i), xpath);
@@ -70,8 +72,6 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
                 once = true;
             }
 
-            // TODO: merge the variable definitions instead
-
             if (variableMap.containsKey(name)) {
                 throw new RuntimeException("Variable \"" + name + "\" defined more than once");
             }
@@ -79,7 +79,7 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
             String valueString = XMLUtils.parseString(random, nodeList.item(i), xpath);
             String[] values = StringUtils.split(valueString, stringSeparator);
 
-            variableMap.put(name, new RandomStringArray(values, once));
+            variableMap.put(name, new RandomStrings(values, once));
         }
 
         setVariableMap(variableMap);
@@ -87,15 +87,15 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
 
     /**
      * Recursively replaces all variables in the given string by a randomly chosen value from the corresponding variable map values.
-     * 
+     *
      * @param random the random generator to use
      * @param string the string to perform replacements in
      * @param variableMap the variable map
-     * 
+     *
      * @return the string with all variables replaced
      */
 
-    public static String replaceVariables(Random random, String string, Map<String, RandomStringArray> variableMap) {
+    public static String replaceVariables(Random random, String string, Map<String, RandomStrings> variableMap) {
         if (string.indexOf('$') < 0) {
             // string contains no variables, return it unchanged
             return string;
@@ -105,14 +105,13 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
         StringBuffer sb = new StringBuffer();
 
         while (matcher.find()) {
-            matcher.appendReplacement(sb, "");
-            RandomStringArray rsa = variableMap.get(matcher.group(1));
+            RandomStrings strings = variableMap.get(matcher.group(1));
 
-            if (rsa == null) {
+            if (strings == null) {
                 throw new RuntimeException("Variable \"" + matcher.group(1) + "\" is invalid");
             }
 
-            sb.append(getRandomString(random, rsa));
+            matcher.appendReplacement(sb, Matcher.quoteReplacement(strings.next(random)));
         }
 
         matcher.appendTail(sb);
@@ -120,56 +119,24 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
         return replaceVariables(random, sb.toString(), variableMap);
     }
 
-    /**
-     * Returns a random string from the given RandomStringArray.
-     * 
-     * @param random the random generator to use
-     * @param randomList the RandomList
-     * 
-     * @return the random string
-     */
-
-    private static String getRandomString(Random random, RandomStringArray randomList) {
-        if (randomList.selectOnce) {
-            String[] strings = randomList.strings;
-            int remaining = randomList.remaining;
-
-            if (remaining <= 0) {
-                remaining = strings.length;
-            }
-
-            int offset = random.nextInt(remaining);
-            remaining--;
-
-            String value = strings[offset];
-            String otherValue = strings[remaining];
-
-            strings[remaining] = value;
-            strings[offset] = otherValue;
-
-            randomList.remaining = remaining;
-
-            return value;
-        } else {
-            return randomList.strings[random.nextInt(randomList.strings.length)];
-        }
-    }
-
-    public Map<String, RandomStringArray> getVariableMap() {
+    public Map<String, RandomStrings> getVariableMap() {
         return variableMap;
     }
 
-    public void setVariableMap(Map<String, RandomStringArray> variableMap) {
+    public void setVariableMap(Map<String, RandomStrings> variableMap) {
         this.variableMap = variableMap;
     }
 
     /**
-     * Container for random string arrays.
+     * Container for random strings.
      */
 
-    private static class RandomStringArray {
+    private static class RandomStrings {
         /** The array of strings. */
         private String[] strings;
+
+        /** The number of strings. */
+        private int size;
 
         /** The number of unused strings. */
         private int remaining;
@@ -177,9 +144,55 @@ public class CFGSongNameEngine extends AbstractSongNameEngine {
         /** True if each string should be used only once. */
         private boolean selectOnce = true;
 
-        public RandomStringArray(String[] strings, boolean selectOnce) {
+        /**
+         * Constructor.
+         *
+         * @param strings the array of strings
+         * @param selectOnce true if each string should be returned once
+         */
+
+        public RandomStrings(String[] strings, boolean selectOnce) {
             this.strings = strings;
+            this.size = strings.length;
             this.selectOnce = selectOnce;
+        }
+
+        /**
+         * Returns the number of random strings.
+         *
+         * @return the number of random strings
+         */
+
+        public int size() {
+            return size;
+        }
+
+        /**
+         * Returns the next random string.
+         * @param random the random generator
+         *
+         * @return the next random string
+         */
+
+        public String next(Random random) {
+            if (selectOnce) {
+                if (remaining <= 0) {
+                    remaining = size;
+                }
+
+                int offset = random.nextInt(remaining);
+                remaining--;
+
+                String value = strings[offset];
+                String otherValue = strings[remaining];
+
+                strings[remaining] = value;
+                strings[offset] = otherValue;
+
+                return value;
+            } else {
+                return strings[random.nextInt(size)];
+            }
         }
     }
 }

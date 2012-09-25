3fa66d361d65938d916dd59aba4a6fae30309fe0
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2012-09-25 12:04:07 +0000

    Added missing Component interface.

---
+++ soundhelix/src/com/soundhelix/misc/Component.java
@@ -0,0 +1,10 @@
+package com.soundhelix.misc;
+
+/**
+ * Convenience interface.
+ *
+ * @author Thomas Schuerger (thomas@schuerger.com)
+ */
+
+public interface Component extends XMLConfigurable, RandomSeedable {
+}

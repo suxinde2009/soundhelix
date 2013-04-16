07390ca02cdc6c46cf3baf80818b198d63aeb217
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2013-04-16 14:04:28 +0000

    Added ability to get/set/remove custom attributes from/in/from the song context.

--- soundhelix/src/com/soundhelix/misc/SongContext.java
+++ soundhelix/src/com/soundhelix/misc/SongContext.java
@@ -1,10 +1,15 @@
 package com.soundhelix.misc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.soundhelix.component.player.Player;
 
 /**
  * Defines the context of a song. During song generation, the context is enriched with information.
  * Each of the fields can only be set to at most once.
+ * 
+ * Custom attributes can be set in the song context to pass custom data along between components.
  * 
  * @author Thomas Schuerger (thomas@schuerger.com)
  */
@@ -31,11 +36,18 @@ public class SongContext {
     /** The player. */
     private Player player;
 
+    /** The attribute map (initialized lazily). */
+    private Map<String, Object> attributeMap;
+    
     public Long getRandomSeed() {
         return randomSeed;
     }
 
     public void setRandomSeed(Long randomSeed) {
+        if (this.randomSeed != null) {
+            throw new IllegalArgumentException("randomSeed already set");
+        }
+        
         this.randomSeed = randomSeed;
     }
 
@@ -110,4 +122,63 @@ public class SongContext {
 
         this.player = player;
     }
+    
+    /**
+     * Returns the attribute with the given name. If the attribute doesn't exist or it's value is null, null is returned.
+     * 
+     * @param name the attribute name
+     *
+     * @return the attribute value
+     */
+    
+    public Object getAttribute(String name) {
+        return getAttributeMap().get(name);
+    }
+
+    /**
+     * Sets the attribute to the given value. If the attribute is already set, it is replaced with the new value.
+     * 
+     * @param name the attribute name
+     * @param value the attribute value
+     */
+    
+    public void setAttribute(String name, Object value) {
+        getAttributeMap().put(name, value);
+    }
+
+    /**
+     * Checks if the given attribute has been set. Can be used to distinguish null values from non-existing values.
+     * 
+     * @param name the attribute name
+     *
+     * @return true if the attribute is set, false otherwise
+     */
+    
+    public boolean hasAttribute(String name) {
+        return getAttributeMap().containsKey(name);
+    }
+
+    /**
+     * Removes the given attribute. If the attribute is not set, nothing is done.
+     * 
+     * @param name the attribute name
+     */
+    
+    public void removeAttribute(String name) {
+        getAttributeMap().remove(name);
+    }
+
+    /**
+     * Returns the attribute map. The map is created if it doesn't exist yet.
+     * 
+     * @return the attribute map
+     */
+    
+    private Map<String, Object> getAttributeMap() {
+        if (attributeMap == null) {
+            attributeMap = new HashMap<String, Object>();
+        }
+        
+        return attributeMap;
+    }    
 }

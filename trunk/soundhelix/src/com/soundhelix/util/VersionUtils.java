ef03bc2adf857ef71caf8d828bb1fe515107af55
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2013-07-15 09:23:16 +0000

    Added more complex application version check against a version pattern.

--- trunk/soundhelix/src/com/soundhelix/util/VersionUtils.java
+++ trunk/soundhelix/src/com/soundhelix/util/VersionUtils.java
@@ -1,5 +1,8 @@
 package com.soundhelix.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.apache.log4j.Logger;
 
 import com.soundhelix.constants.BuildConstants;
@@ -14,6 +17,30 @@ public final class VersionUtils {
     /** The logger. */
     private static final Logger LOGGER = Logger.getLogger(new Throwable().getStackTrace()[0].getClassName());
 
+    /** The version pattern string. */
+    private static final String VERSION = "-?\\d+(?:\\.-?\\d+)*u?";
+     
+     /** The version pattern. */
+    private static final Pattern VERSION_PATTERN = Pattern.compile(VERSION);
+    
+    /** The version plus pattern string. */
+    private static final String VERSION_PLUS = "(" + VERSION + ")\\+";
+    
+    /** The version plus pattern. */
+    private static final Pattern VERSION_PLUS_PATTERN = Pattern.compile(VERSION_PLUS);
+    
+    /** The version range pattern string. */
+    private static final String VERSION_RANGE = "(" + VERSION + ")-(" + VERSION + ")";
+    
+    /** The version range pattern. */
+    private static final Pattern VERSION_RANGE_PATTERN = Pattern.compile(VERSION_RANGE);
+    
+    /** The version alternatives pattern string. */
+    private static final String VERSION_ALTERNATIVES = "(?:" + VERSION + "|" + VERSION_PLUS + "|" + VERSION_RANGE + ")";
+    
+    /** The versions pattern. */
+    private static final Pattern VERSIONS_PATTERN = Pattern.compile(VERSION_ALTERNATIVES + "(?:," + VERSION_ALTERNATIVES + ")*");
+    
     /**
      * Private constructor.
      */
@@ -39,7 +66,52 @@ public final class VersionUtils {
         return "SoundHelix " + BuildConstants.VERSION + " (r" + BuildConstants.REVISION + "), built on " + BuildConstants.BUILD_DATE;
     }
 
-
+    /**
+     * Compares the version string against a version pattern. If the version matches the pattern, true will be returned, otherwise false will be
+     * returned. The pattern is a comma-separated list of version specifications, where each is one of "<version>", "<version>+",
+     * and "<version1>-<version2>". If any version specification matches, the check is considered as successful.
+     *  
+     * @param version the version
+     * @param versionPattern the version pattern
+     * @return true if the pattern matches, false otherwise
+     */
+    
+    public static boolean checkVersion(String version, String versionPattern) {
+        int[] versionInts = parseVersionString(version);
+        
+        if (!VERSIONS_PATTERN.matcher(versionPattern).matches()) {
+            throw new RuntimeException("Error in version pattern \"" + versionPattern + "\"");
+        }
+        
+        String[] versionPatternParts = versionPattern.split(",");
+        
+        for (String versionPatternPart : versionPatternParts) {
+            if (VERSION_PATTERN.matcher(versionPatternPart).matches()) {
+                if (compareVersions(versionInts, versionPatternPart) == 0) {
+                    return true;
+                }
+            } else if (VERSION_PLUS_PATTERN.matcher(versionPatternPart).matches()) {
+                if (compareVersions(versionInts, versionPatternPart.substring(0, versionPatternPart.length() - 1)) >= 0) {
+                    return true;
+                }
+            } else {
+                Matcher m = VERSION_RANGE_PATTERN.matcher(versionPatternPart);
+                
+                if (m.matches()) {
+                    String version1 = m.group(1);
+                    String version2 = m.group(2);
+                    
+                    if (compareVersions(versionInts, version1) >= 0 && compareVersions(versionInts, version2) <= 0) {
+                        return true;
+                    }
+                    
+                }
+            }            
+        }
+        
+        return false;
+    }
+    
     /**
      * Compares the two given version strings. Returns a negative number if the first is smaller than the second, a positive number of the first is
      * greater than the second, 0 if they are equal.
@@ -51,10 +123,21 @@ public final class VersionUtils {
      */
 
     public static int compareVersions(String version1, String version2) {
-        int[] versionInts1 = parseVersionString(version1);
-        int[] versionInts2 = parseVersionString(version2);
+        return compareVersions(parseVersionString(version1),  parseVersionString(version2));
+    }
 
-        return compareVersions(versionInts1, versionInts2);
+    /**
+     * Compares the two given version strings. Returns a negative number if the first is smaller than the second, a positive number of the first is
+     * greater than the second, 0 if they are equal.
+     *
+     * @param versionInts1 the first version int array
+     * @param version2 the second version string
+     *
+     * @return an integer determining the relative order of the two versions
+     */
+
+    public static int compareVersions(int[] versionInts1, String version2) {
+        return compareVersions(versionInts1, parseVersionString(version2));
     }
 
     /**

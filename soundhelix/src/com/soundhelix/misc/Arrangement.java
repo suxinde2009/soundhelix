6c7c7c7c7c0750e4edb0319ce4a2fdaf143b95e7
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2014-06-09 11:31:06 +0000

    Added first version of conditional LFO support.

--- soundhelix/src/com/soundhelix/misc/Arrangement.java
+++ soundhelix/src/com/soundhelix/misc/Arrangement.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -22,6 +24,9 @@ import java.util.zip.GZIPOutputStream;
 public class Arrangement implements Iterable<Arrangement.ArrangementEntry> {
     /** The list of arrangement entries. */
     private final List<ArrangementEntry> entryList = new ArrayList<ArrangementEntry>();
+
+    /** The map that maps instruments to arrangement entries. */
+    private final Map<String, ArrangementEntry> entryMap = new HashMap<String, ArrangementEntry>();
 
     /**
      * Constructor.
@@ -38,7 +43,13 @@ public class Arrangement implements Iterable<Arrangement.ArrangementEntry> {
      */
 
     public void add(Track track, String instrument) {
-        entryList.add(new ArrangementEntry(track, instrument));
+        if (entryMap.containsKey(instrument)) {
+            throw new IllegalArgumentException("Instrument \"" + instrument + "\" already in arrangement");
+        }
+        
+        ArrangementEntry entry = new ArrangementEntry(track, instrument);
+        entryList.add(entry);
+        entryMap.put(instrument, entry);
     }
 
     @Override
@@ -66,6 +77,18 @@ public class Arrangement implements Iterable<Arrangement.ArrangementEntry> {
     
     public ArrangementEntry get(int index) {
         return entryList.get(index);
+    }
+
+    /**
+     * Returns the arrangement entry whose instrument has the given name. If such an entry doesn't exist, null is returned.
+     * 
+     * @param instrument the name of the instrument
+     *
+     * @return the arrangement entry or null
+     */
+    
+    public ArrangementEntry get(String instrument) {
+        return entryMap.get(instrument);
     }
 
     /**

984ab724ca5dc8963fea99513fd54dbaebab6059
trancetip <trancetip@2a610bfd-e773-4f87-915c-491094d88175>
2016-10-28 20:38:29 +0000

    Removed deprecated tag obeyChordSubtype from various SequenceEngine implementations.

--- trunk/soundhelix/src/com/soundhelix/component/sequenceengine/impl/ArpeggioSequenceEngine.java
+++ trunk/soundhelix/src/com/soundhelix/component/sequenceengine/impl/ArpeggioSequenceEngine.java
@@ -191,11 +191,6 @@ public class ArpeggioSequenceEngine extends AbstractSequenceEngine {
         }
 
         try {
-            setNormalizeChords(!XMLUtils.parseBoolean(random, "obeyChordSubtype", node));
-            logger.warn("The tag \"obeyChordSubtype\" has been deprecated. Use \"normalizeChords\" with inverted value instead.");
-        } catch (Exception e) {}
-
-        try {
             setNormalizeChords(XMLUtils.parseBoolean(random, "normalizeChords", node));
         } catch (Exception e) {}
 
